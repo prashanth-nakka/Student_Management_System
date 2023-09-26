580f71394cc40dcb1402b1ce1d8363b31e7e3689
prashanth97 <prashanthnakk97@gmail.com>
2023-09-26 22:28:17 +0100

    Template Setup, Addeed CSS and JS libraries, loaded sttaic files.

--- Student_Management_System/urls.py
+++ Student_Management_System/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from Student_Management_System import settings
+from Student_Management_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('demo/', views.showDemoPage),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
